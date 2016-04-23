f3d7382a67841ffd68395f905f6b88ceb9912371
Vlad Shudrov <vlad.shudrov@gmail.com>
2016-04-23 13:58:53 +0600

    comments, unit test

--- MyersDiff/Snake.cs
+++ MyersDiff/Snake.cs
@@ -4,12 +4,17 @@ using System;
 namespace MyersDiff
 {
     /// <summary>
-    /// Согласно Майерсу, змейка - это последовательность длины D-1, оканчивающаяся операцией вставки или удаления.
+    /// Согласно Майерсу, змейка - это путь длиной D-1, начинающийся с операции вставки или удаления, и оканчивающийся последовательностью диагональных шагов (возможно пустой).
     /// </summary>
     internal sealed class Snake
     {
         /// <summary>
-        /// Количество шагов по диагонали в теущей змейке
+        /// D - число операций удаления или вставки для этой змейки
+        /// </summary>
+        public long D { get; }
+
+        /// <summary>
+        /// Количество шагов по диагонали в текущей змейке
         /// </summary>
         public long Diagonals { get; }
 
@@ -33,15 +38,27 @@ namespace MyersDiff
         /// </summary>
         public LongPoint Start { get; }
 
-        public Snake(OperationKind operationKind, long xStart, long yStart, long xEnd, long yEnd, long diagonals, IComparable element)
+        public Snake(OperationKind operationKind, long xStart, long yStart, long xEnd, long yEnd, long d, long diagonals, IComparable element)
         {
             this.Element = element;
             this.OperationKind = operationKind;
             this.End = new LongPoint(xEnd, yEnd);
             this.Start = new LongPoint(xStart, yStart);
             this.Diagonals = diagonals;
+            this.D = d;
         }
 
+        /// <summary>
+        /// Вычисление змейки
+        /// </summary>
+        /// <param name="v">Массив V</param>
+        /// <param name="k">Диагональ, относительно которой ищем максимально протяженный D-путь</param>
+        /// <param name="d">Длина D-пути</param>
+        /// <param name="a">Оригинал</param>
+        /// <param name="n">Длина оригинала</param>
+        /// <param name="b">Источник с изменениями</param>
+        /// <param name="m">Длина источника с изменениями</param>
+        /// <returns></returns>
         public static Snake CalculateSnake(VArray v, long k, long d, ISource a, long n, ISource b, long m)
         {
             var down = (k == -d || (k != d && v[k - 1] < v[k + 1]));
@@ -54,13 +71,13 @@ namespace MyersDiff
             var snake = 0;
             while (xEnd < n && yEnd < m && Equals(a[xEnd], b[yEnd])) { xEnd++; yEnd++; snake++; }
             var snk = new Snake(yStart < 0 ? OperationKind.Equal : (down ? OperationKind.Insert : OperationKind.Delete),
-                                xStart, yStart, xEnd, yEnd, snake, down ? (yStart < 0 ? ' ' : b[yStart]) : a[xStart]);
+                                xStart, yStart, xEnd, yEnd, d, snake, down ? (yStart < 0 ? ' ' : b[yStart]) : a[xStart]);
             return snk;
         }
 
         public override string ToString()
         {
-            return $"{this.OperationKind} Start: {this.Start} End: {this.End} {(this.OperationKind == OperationKind.Equal ? string.Empty : $"Element: {this.Element}")} Diagonals: {this.Diagonals}";
+            return $"{this.OperationKind} Start: {this.Start} End: {this.End} {(this.OperationKind == OperationKind.Equal ? string.Empty : $"Element: {this.Element}")} D:{this.D} Diagonals: {this.Diagonals}";
         }
     }
 }
---
+++ MyersDiffTest/MyersDiffUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyersDiffTest
+{
+    [TestClass]
+    public class MyersDiffUnitTest
+    {
+        [TestMethod]
+        public void TestSingleEmptySource()
+        {
+        }
+    }
+}
